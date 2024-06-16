21ce7cd23e9777883d288cb9dd82d37600059ef7
Dinara <dinarazamatova@gmail.com>
2024-06-17 00:08:21 +0500

    user interaction

---
+++ script.js
@@ -0,0 +1,48 @@
+// 1
+// let name = prompt("Напишите пожалуйста ваше имя"), age = prompt("Напишите ваш возраст")
+// alert(name + " " + age)
+ 
+// 2
+//  let num1 = prompt("Write number1: "), num2 = prompt("Write number2: ");
+//  console.log(num1 % num2)
+
+// 3
+// let choice = confirm("Do you agree to receive notification ?");
+// console.log(choice)
+
+// 4
+// let age = prompt("How old are you?")
+// console.log(Boolean(age))
+
+// 5
+// let number = prompt("Write number: ")
+// alert(number ** 3)
+
+// 6
+// let text1 = prompt("Write something..."), text2 = prompt("Write something else...");
+// alert(text1 + " " + text2)
+
+// 7 
+// let numb1 = prompt("Write number one: "), numb2 = prompt("Write number two: "), numb3 = prompt("Write number three: ");
+// let average = (Number(numb1) + Number(numb2) + Number(numb3)) / 3;
+// alert("Average is: " + average)
+
+// 8
+// let hour = prompt("Write a hour: ");
+// let minut = prompt("Write a minute: ");
+// let second = prompt("Write a second: ");
+// let total = ((hour * 3600) + (minut * 60) + Number(second));
+// alert(total)
+
+// 9
+// let celsia = prompt("Write a temperature in Celsia: ");
+// let farengeit = (celsia * 9/5) + 32;
+// let kelvin = Number(celsia) + 273.15;
+ 
+// alert("Temperature in Farengeit: " + farengeit);
+// alert("Temperature in Kelvin: " + kelvin);
+
+// 10
+// let radius = prompt("Please, write radius: ");
+// const pi = 3.14;
+// alert(pi * radius ** 2)
